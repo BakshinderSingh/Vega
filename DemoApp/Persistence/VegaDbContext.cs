6f82644fcdb1dd4f13694cc4a91d0b0ff44be36c
Bakshinder <bakshinder@live.com>
2018-08-14 07:24:28 +0530

    upto data annotations

---
+++ DemoApp/Persistence/VegaDbContext.cs
@@ -0,0 +1,21 @@
+﻿using DemoApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoApp.Persistence
+{
+    public class VegaDbContext:DbContext
+    {
+        public VegaDbContext(DbContextOptions<VegaDbContext> options)
+            :base(options)
+        {
+            
+        }
+
+        public DbSet<Make> Makes { get; set; }
+
+    }
+}
